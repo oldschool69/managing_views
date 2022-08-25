6bc13798a957db36a83eb93c90100a676c7d8807
Flavio Oliveira <flavio.marcondes@gmail.com>
2022-08-25 16:40:13 -0300

    Continuing development

---
+++ src/json_serialization/to_do_items.rs
@@ -0,0 +1,11 @@
+use serde::Serialize;
+use crate::to_do::ItemTypes;
+use crate::to_do::structs::base::Base;
+
+#[derive(Serialize)]
+pub struct ToDoItems {
+    pub pending_items: Vec<Base>,
+    pub done_items: Vec<Base>,
+    pub pending_items_count: i8,
+    pub done_items_count: i8
+}
